4a9c1c4ae1a97c79e71cc2adf0f67924f0a3ae08
averangeall <averangeall@gmail.com>
2014-10-12 20:50:12 +0800

    Remove Django related files

--- crowd_opinion/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('',
-    url(r'^$', 'crowd_opinion.views.home'),
-    url(r'^district/(?P<district_name>.+)$', 'crowd_opinion.views.district'),
-    url(r'^candidate/(?P<candidate_name>.+)$', 'crowd_opinion.views.candidate'),
-    url(r'^about/stars/$', 'crowd_opinion.views.about_stars'),
-)
